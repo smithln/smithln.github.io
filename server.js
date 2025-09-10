a0e7f5d130ec7ca1312e73e85ad3ef537b296351
smithln <laurennicoleott@gmail.com>
2025-09-10 14:48:14 -0700

    Adds server

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+// set view engine
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+// serve static files (css, images)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// route
+app.get('/', (req, res) => {
+  res.render('index', { name: 'Lauren', role: 'Design Director' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
